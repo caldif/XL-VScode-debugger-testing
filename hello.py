23396b62230d7c3b44a07ff989423a7376e75112
Adin Zimmerman <adinz@email.unc.edu>
2023-01-29 15:33:56 -0500

    first code added

---
+++ hello.py
@@ -0,0 +1,11 @@
+greeting: str = "hello world"
+
+goodbye: str = "bye"
+
+print(greeting) #breakpoint here to show how the vairables pop out
+#have them also mess with the variables in debug terminal to see how that affects print
+
+#logpoint to just print goodbye
+greeting = greeting + ', ' + goodbye 
+
+print(greeting) #breakpoint here to show the vairables changing
